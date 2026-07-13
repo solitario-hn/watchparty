3fdb1e2a2745a2578ee6dd62db8310cfd989bdbd
solitario-hn <elsolitariolafe@gmail.com>
2026-07-13 19:47:02 +0530

    adding volume controls

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,10 +1,30 @@
-import { FishingHook, Ghost } from "lucide-react";
+import {
+  HugeiconsFreeIcons,
+  PauseIcon,
+  PlayCircleIcon,
+  PlayIcon,
+  VolumeHighFreeIcons,
+  VolumeMute02FreeIcons,
+} from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
+import {
+  CirclePause,
+  CirclePlay,
+  FishingHook,
+  Ghost,
+  Play,
+  PlayOff,
+} from "lucide-react";
 import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 
 export default function Video() {
   const [link, isLinked] = useState("");
-  const [videoUrl, setVideoUrl] = useState("");
+  const [videoUrl, setVideoUrl] = useState(
+    "https://youtu.be/nBv5BzFpvvE?si=52of4txwdXj7kTzf",
+  );
+  const [playing, isPlaying] = useState(false);
+  const [muted, isMuted] = useState(false);
 
   return (
     <div className="h-full items-center justify-center w-full flex">
@@ -31,12 +51,43 @@ export default function Video() {
           </button>
         </div>
       ) : (
-        <ReactPlayer
-          width={"100%"}
-          height={"100%"}
-          controls={true}
-          src={videoUrl}
-        />
+        <div className="flex flex-col h-full w-full ">
+          <ReactPlayer
+            width={"100%"}
+            height={"100%"}
+            controls={true}
+            src={videoUrl}
+          />
+          <div className="flex flex-row items-center w-full h-10 px-2 p-2 justify-between bg-[#1e1e2e]">
+            <button
+              onClick={() => {
+                isPlaying(!playing);
+              }}
+              className="text-[#6b7180] text-2xl w-7 h-7 transition-all duration-300 hover:text-blue-300 cursor-pointer hover:scale-105 hover:-translate-y-0.75"
+            >
+              <HugeiconsIcon icon={playing ? PlayIcon : PauseIcon} />
+            </button>
+            <div className="flex items-center group/volume ">
+              <button
+                onClick={() => {
+                  isMuted(!muted);
+                }}
+                className="text-[#6b7180] text-2xl w-7 h-7 transition-all duration-300 hover:text-blue-300 cursor-pointer hover:scale-105 hover:-translate-y-0.75"
+              >
+                <HugeiconsIcon
+                  icon={muted ? VolumeMute02FreeIcons : VolumeHighFreeIcons}
+                />
+              </button>
+              <input
+                className="w-0 bg-[#8b9ecf] h-1 rounded-lg opacity-0 transition-all duration-300 group-hover/volume:w-16 group-hover/volume:opacity-100 accent-blue-300 focus:accent-blue-400  appearance-none focus:outline-none"
+                type="range"
+                min={0}
+                max={1}
+                step={0.05}
+              />
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
